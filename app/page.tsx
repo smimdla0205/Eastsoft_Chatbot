9a2b3928d2a14ea47585052aad3fc45678b5cdf9
smimdla0205 <sangmin0205@gmail.com>
2025-11-14 14:36:00 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { AIChatbot } from "@/components/AIChatbot";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+  <AIChatbot />
+    </div>
+  );
+}
